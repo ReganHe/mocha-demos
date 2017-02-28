5d1d336c7250425e257cb2338688f5cd53e6847f
何柯 <heke@hujiang.com>
2017-02-28 17:51:14 +0800

    update

--- demo01/add.test.js
+++ demo01/add.test.js
@@ -1,8 +1,12 @@
 var add = require('./add.js');
 var expect = require('chai').expect;
 
-describe('加法函数的测试', function() {
-  it('1 加 1 应该等于 2', function() {
-    expect(add(1, 1)).to.be.equal(2);
-  });
+describe('加法函数的测试', function () {
+	it('1 加 1 应该等于 2', function () {
+		expect(add(1, 1)).to.be.equal(2);
+	});
+
+	it('1 加 2 应该等于 3', function () {
+		expect(add(1, 2)).to.be.equal(3);
+	});
 });
--- demo05/timeout.test.js
+++ demo05/timeout.test.js
@@ -1,15 +1,16 @@
 var expect = require('chai').expect;
 
-describe('timeout.test.js - 超时测试', function() {
-  it('测试应该 5000 毫秒后结束', function(done) {
-    var x = true;
-    var f = function() {
-      x = false;
-      expect(x).to.be.not.ok;
-      done();
-    };
-    setTimeout(f, 4000);
-  });
+//Mocha默认每个测试用例最多执行2000毫秒
+describe('timeout.test.js - 超时测试', function () {
+	it('测试应该 5000 毫秒后结束', function (done) {
+		var x = true;
+		var f = function () {
+			x = false;
+			expect(x).to.be.not.ok;
+			done();
+		};
+		setTimeout(f, 4000);
+	});
 });
 
 
